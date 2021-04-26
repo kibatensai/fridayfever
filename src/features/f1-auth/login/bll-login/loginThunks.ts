4a8fd0f4dfe72e008557120b56931f9befcf4a8d
Kibatensai <lenutd@yandex.ru>
2021-04-26 17:04:30 +0300

    simple profile rendering

--- src/features/f1-auth/login/bll-login/loginThunks.ts
+++ src/features/f1-auth/login/bll-login/loginThunks.ts
@@ -1,47 +1,76 @@
-import { ProfileActionsType } from './../../profile/bll-profile/profileActions';
-import { LoginActions, LoginActionsType } from './loginActions';
-import { ThunkAction } from 'redux-thunk';
-import { AppStoreType } from '../../../../main/bll/store';
-import { loginAPI, LoginDetailsType } from '../dal-login/loginInstance';
+import { ThunkAction } from "redux-thunk";
+import { AppStoreType } from "../../../../main/bll/store";
+import { loginAPI, LoginDetailsType } from "../dal-login/loginInstance";
+import {
+  ProfileActions,
+  ProfileActionsType
+} from "./../../profile/bll-profile/profileActions";
+import { LoginActions, LoginActionsType } from "./loginActions";
 
 export const signIn = (
   data: LoginDetailsType
-): ThunkAction<void, AppStoreType, unknown, LoginActionsType | ProfileActionsType> => async (dispatch) => {
+): ThunkAction<
+  void,
+  AppStoreType,
+  unknown,
+  LoginActionsType | ProfileActionsType
+> => async (dispatch) => {
   dispatch(LoginActions.setLoading(true));
   try {
     const res = await loginAPI.signIn(data);
+    dispatch(ProfileActions.setUser(res.data));
     dispatch(LoginActions.setSuccess(true));
     dispatch(LoginActions.setLoading(false));
   } catch (e) {
-    const error = e.response ? e.response.data.error : e.message + ', more details in the console';
+    const error = e.response
+      ? e.response.data.error
+      : e.message + ", more details in the console";
     dispatch(LoginActions.setLoading(false));
     dispatch(LoginActions.setError(error));
   }
 };
 
-export const signOut = (): ThunkAction<void, AppStoreType, unknown, LoginActionsType> => async (dispatch) => {
+export const signOut = (): ThunkAction<
+  void,
+  AppStoreType,
+  unknown,
+  LoginActionsType
+> => async (dispatch) => {
   dispatch(LoginActions.setLoading(true));
   try {
     const res = await loginAPI.signOut();
     dispatch(LoginActions.setLoading(false));
   } catch (e) {
-    const error = e.response ? e.response.data.error : e.message + ', more details in the console';
+    const error = e.response
+      ? e.response.data.error
+      : e.message + ", more details in the console";
     dispatch(LoginActions.setLoading(false));
     dispatch(LoginActions.setError(error));
   }
 };
 
-export const me = (): ThunkAction<void, AppStoreType, unknown, LoginActionsType> => async (dispatch) => {
+export const me = (): ThunkAction<
+  void,
+  AppStoreType,
+  unknown,
+  LoginActionsType | ProfileActionsType
+> => async (dispatch) => {
   dispatch(LoginActions.setLoading(true));
   try {
     const res = await loginAPI.me();
-    if(res.status === 200) {
-    dispatch(LoginActions.setLoading(false));
-    dispatch(LoginActions.setSuccess(true))
-    dispatch(LoginActions.setUserId(res.data._id))
+
+    if (res.status === 200) {
+      console.log(res);
+
+      dispatch(ProfileActions.setUser(res.data));
+      dispatch(LoginActions.setLoading(false));
+      dispatch(LoginActions.setSuccess(true));
+      dispatch(LoginActions.setUserId(res.data._id));
     }
   } catch (e) {
-    const error = e.response ? e.response.data.error : e.message + ', more details in the console';
+    const error = e.response
+      ? e.response.data.error
+      : e.message + ", more details in the console";
     dispatch(LoginActions.setLoading(false));
     dispatch(LoginActions.setError(error));
   }
